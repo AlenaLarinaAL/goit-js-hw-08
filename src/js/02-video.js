9e6961f5615540a05fbb2d86d603062f89771354
AlenaLarinaAL <alenalarina605@gmail.com>
2023-03-24 16:14:57 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+console.log(Player);
+
+const options = {
+    width: 640,
+    loop: true,
+}
+
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl, options);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const setPlayerTime = function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+}
+
+player.setVolume(0.4);
+
+player.on('timeupdate', throttle(setPlayerTime, 1500));
+
+const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+function checkCurrentTime() {
+    if (!savedTime) {
+        return;
+    } else {
+        player.setCurrentTime(savedTime);
+    }
+}
+
+checkCurrentTime();
+
+// localStorage.clear();
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const email = formEl.elements.email;
+const message = formEl.elements.message;
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+function setData() {
+    const setItems = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(setItems));
+};
+
+formEl.addEventListener('input', setData);
+
+function checkData() {
+    const elements = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (elements) {
+        const { email, message } = JSON.parse(elements);
+        email.value = email;
+        message.value = message;
+        console.log({ email, message });
+    }
+
+};
+
+const onSubmit = throttle(saveData, 500);
+formEl.addEventListener('submit', onSubmit);
+
+function saveData(event) {
+    event.preventDefault();
+    const setItems = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(setItems));
+    console.log(setItems);
+    email.value = '';
+    message.value = '';
+    localStorage.clear();
+}
